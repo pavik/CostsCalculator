e8abd1915744f032ef807f204d5fc2e59b620029
Aliaksei Plashchanski <alekseyploschanskiy@yahoo.com>
2013-01-04 16:55:52 +0300

    test sql scripts have added

---
+++ ClientAndroid/db/cost_items_test_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO cost_items (ci_guid, ci_name) VALUES('1', 'Car');
+INSERT INTO cost_items (ci_guid, ci_name) VALUES('2', 'Mobile');
+INSERT INTO cost_items (ci_guid, ci_name) VALUES('3', 'Credit');
