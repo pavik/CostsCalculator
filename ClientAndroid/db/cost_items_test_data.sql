7dd1ed786206d3710945077991c555ba0666cf6b
Aliaksei Plashchanski <alekseyploschanskiy@yahoo.com>
2013-07-30 11:17:10 +0300

    db folder structure refactoring

--- ClientAndroid/db/cost_items_test_data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO cost_items (ci_guid, ci_name) VALUES('1', 'Car');
-INSERT INTO cost_items (ci_guid, ci_name) VALUES('2', 'Mobile');
-INSERT INTO cost_items (ci_guid, ci_name) VALUES('3', 'Credit');
