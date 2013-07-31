23d7ffb10d84b67cd899adc2d00c662aa06d9fec
Aliaksei Plashchanski <alekseyploschanskiy@yahoo.com>
2013-07-31 11:57:54 +0300

    table versions added to scripts

--- ClientAndroid/Source/assets/createdb.sql
+++ ClientAndroid/Source/assets/createdb.sql
@@ -4,6 +4,8 @@ CREATE TRIGGER cost_items_after_update AFTER UPDATE ON cost_items BEGIN UPDATE c
 CREATE TABLE cost_item_records (cir_id INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL UNIQUE, cir_guid VARCHAR NOT NULL UNIQUE, cir_ci_guid VARCHAR NOT NULL, cir_datetime DATETIME NOT NULL, cir_sum DOUBLE NOT NULL, cir_currency VARCHAR, cir_tag VARCHAR, cir_comment VARCHAR, cir_audit_ct DATETIME NOT NULL  DEFAULT CURRENT_TIMESTAMP, cir_audit_version INTEGER NOT NULL  DEFAULT 0, cir_audit_mt DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP);
 CREATE TRIGGER cost_items_records_after_update AFTER UPDATE ON cost_item_records BEGIN UPDATE cost_item_records SET cir_audit_version = cir_audit_version+1, cir_audit_mt = CURRENT_TIMESTAMP WHERE cir_id = OLD.cir_id AND cir_id = NEW.cir_id; END;
 
+CREATE TABLE versions (ver_id INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , ver_number INTEGER NOT NULL  UNIQUE , ver_audit_ct DATETIME NOT NULL  DEFAULT CURRENT_TIMESTAMP);
+
 CREATE  INDEX cir_index_ci_guid_asc ON cost_item_records (cir_ci_guid ASC);
 CREATE  INDEX cir_index_datetime_asc ON cost_item_records (cir_datetime ASC);
 CREATE  INDEX cir_index_datetime_desc ON cost_item_records (cir_datetime DESC);
