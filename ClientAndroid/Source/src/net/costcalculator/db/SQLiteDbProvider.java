fd47c7da6f63719c141327e1ddc89f7e3a1a79d7
Aliaksei Plashchanski <alekseyploschanskiy@yahoo.com>
2013-08-16 13:16:37 +0300

    app and db version changed

--- ClientAndroid/Source/src/net/costcalculator/db/SQLiteDbProvider.java
+++ ClientAndroid/Source/src/net/costcalculator/db/SQLiteDbProvider.java
@@ -23,7 +23,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class SQLiteDbProvider extends SQLiteOpenHelper
 {
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
 
     public SQLiteDbProvider(Context context)
     {
