66587613dd014d2d207cbeb448141aa8a6873a72
Aliaksei Plashchanski <alekseyploschanskiy@yahoo.com>
2013-08-28 21:50:51 +0300

    donation page done

--- expensesandroid/common.py
+++ expensesandroid/common.py
@@ -10,7 +10,12 @@
 #-------------------------------------------------------------------------------
 
 index_page = 'index.html'
-donation_link = u'<form class="form-inline" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick"><input type="hidden" name="hosted_button_id" value="E8XCQADY5X94Q">%s<input type="image" src="https://www.paypalobjects.com/en_US/PL/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></form>'
+donation_link = u'<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">\
+  <input type="hidden" name="cmd" value="_s-xclick">\
+  <input type="hidden" name="hosted_button_id" value="ZZ2TAYFKC4224">%s\
+  <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">\
+  <img alt="" border="0" src="https://www.paypalobjects.com/ru_RU/i/scr/pixel.gif" width="1" height="1">\
+  </form>'
 market_link = u'<a href="https://play.google.com/store/apps/details?id=net.costcalculator.activity"><img alt="Android app on Google Play" src="https://developer.android.com/images/brand/en_app_rgb_wo_60.png" /></a>'
 googleplay_link = u'https://play.google.com/store/apps/details?id=net.costcalculator.activity'
 dropbox_link = u'https://www.dropbox.com/s/ssu6fq57ndhhe62/Expenses.apk'
