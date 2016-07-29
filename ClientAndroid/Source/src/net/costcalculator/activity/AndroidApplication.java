286a84afca7ba7d3b40cb62e143c2f07d348542e
Aliaksei Plashchanski <aleksey.ploschanskiy@gmail.com>
2016-07-29 11:45:33 +0300

    AndroidApplication class added to fix ClassNotFoundException on startup

---
+++ ClientAndroid/Source/src/net/costcalculator/activity/AndroidApplication.java
@@ -0,0 +1,22 @@
+package net.costcalculator.activity;
+
+import net.costcalculator.util.LOG;
+import android.app.Application;
+
+public class AndroidApplication extends Application
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        LOG.INITIALIZE();
+        LOG.D("Application started");
+    }
+
+    @Override
+    public void onLowMemory()
+    {
+        super.onLowMemory();
+        LOG.D("Application onLowMemory");
+    }
+}
